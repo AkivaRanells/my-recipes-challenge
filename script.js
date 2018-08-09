861ee237231c4b2a8e031fbc7b00ce2fb0bd5107
Akiva Ranells <akranells@gmail.com>
2018-08-09 13:47:45 +0300

    working yippee

--- script.js
+++ script.js
@@ -21,36 +21,44 @@ var RecipeApp = function () {
     //id's for ingredients
     var ingId = 0;
 
-    var createRecipe = function(name, image){
+    var createRecipe = function (name, image) {
         var recipe = {
             name: name,
-            image: image, 
+            image: image,
             ingredients: [],
             id: recId
         };
 
         //keeps recipe ids unique 
-        recId ++; 
+        recId++;
 
         recipes.push(recipe);
     };
 
-    var createIngredients = function(ingredientText, recipeId){
+    var createIngredients = function (ingredientText, recipeId) {
         //add code
-        ingId ++;
+        ingId++;
         let ingredient = {
             name: ingredientText,
             id: ingId
-        }
-        recipes[recipeId].ingredients.push();
+        };
+        let recipe = _findRecipeById(recipeId);
+        recipe.ingredients.push(ingredient);
     };
-
-    var _getIngredientsHTML = function(recipe){
+    let _findRecipeById = function (recipeId) {
+        for (let i = 0; i < recipes.length; i++) {
+            if (recipes[i].id === recipeId)
+                return recipes[i];
+        }
+    };
+    var _getIngredientsHTML = function (recipe) {
         var recipesHTML = "";
 
         //add code
-        for(let i = 0 ; i < recipe.ingredients.length ; i ++){
-            recipesHTML+='<li>'+recipe.ingredient[i]+'</li>';
+        if (recipe.ingredients.length>0) {
+            for (let i = 0; i < recipe.ingredients.length; i++) {
+                recipesHTML += '<li>' + recipe.ingredients[i]['name'] + '</li>';
+            }
         }
         return recipesHTML;
     };
@@ -59,27 +67,27 @@ var RecipeApp = function () {
         //empty recipes div
         $recipes.empty();
 
-        for(var i = 0; i < recipes.length; i ++){
+        for (var i = 0; i < recipes.length; i++) {
             //current recipe in iteration
-            var recipe = recipes[i];
+            let recipe = recipes[i];
 
             //return HTML for all ingredients
             var ingredients = _getIngredientsHTML(recipe); //add code
 
             $recipes.append(
-                '<div class="recipe col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' + 
-                    '<h4 class="text-capitalize font-italic text-center">' + recipe.name + '</h4>' +
-                    '<img class="recipe-img" src="' + recipe.image + '"/>' +
-                    '<hr>' +
-                    '<h5 class="font-italic font-bold text-center">ingredients</h5>' +
-                    '<div class="input-group mb-3">' +
-                        '<div class="input-group-prepend">' +
-                            '<span class="add-ingredients input-group-text" id="basic-addon3">Add Ingredients</span>' +
-                        '</div>' + 
-                        '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +
-                        
-                    '</div>' +
-                    '<ul class="ingredients">' + ingredients + '</ul>'+
+                '<div class="recipe col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' +
+                '<h4 class="text-capitalize font-italic text-center">' + recipe.name + '</h4>' +
+                '<img class="recipe-img" src="' + recipe.image + '"/>' +
+                '<hr>' +
+                '<h5 class="font-italic font-bold text-center">ingredients</h5>' +
+                '<div class="input-group mb-3">' +
+                '<div class="input-group-prepend">' +
+                '<span class="add-ingredients input-group-text" id="basic-addon3">Add Ingredients</span>' +
+                '</div>' +
+                '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +
+
+                '</div>' +
+                '<ul class="ingredients">' + ingredients + '</ul>' +
                 '</div>'
             );
         }
@@ -98,7 +106,7 @@ var app = RecipeApp();
 //--------EVENTS
 
 //add a recipe
-$('.add-recipe').on('click', function(){
+$('.add-recipe').on('click', function () {
     //collect input text
     var name = $('#recipe-name').val();
     var image = $('#recipe-image').val();
@@ -107,9 +115,10 @@ $('.add-recipe').on('click', function(){
     app.createRecipe(name, image);
     app.renderRecipes();
 });
-$('.recipes').on('click', 'span.add-ingredients', function(){
+$('.recipes').on('click', 'span.add-ingredients', function () {
     let ingredientText = $(this).closest('.input-group-prepend').next('#basic-url').val();
     let recipeId = $(this).closest('.recipe').data().id;
     app.createIngredients(ingredientText, recipeId);
+    app.renderRecipes();
 });
 
